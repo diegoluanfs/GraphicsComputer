cea00ef192e5de02e68ce0630c5357a9fe68119d
Diego Luan <diegoluanfs@gmail.com>
2022-08-23 10:28:10 -0300

    Add Matriz and Vetor

---
+++ GraphicsComputer/Matrizes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsComputer
+{
+    public class Matrizes
+    {
+        public static int[,] CriarMatriz2D()
+        {
+            int[,] matriz2D = new int[2, 2];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    matriz2D[i, j] = i+j;
+                }
+            }
+            return matriz2D;
+        }
+        public static int[,] CriarMatriz3D()
+        {
+            int[,] matriz3D = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matriz3D[i, j] = i+j;
+                }
+            }
+            return matriz3D;
+        }
+    }
+}
---
+++ GraphicsComputer/Vetores.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsComputer
+{
+    public class Vetores
+    {
+        public int[] CriarVetor2D()
+        {
+            int[] vetor2D = new int[2];
+            for(int i = 0; i < 2; i++)
+            {
+                vetor2D[i] = i;
+            }
+            return vetor2D;
+        }
+        public int[] CriarVetor3D()
+        {
+            int[] vetor3D = new int[3];
+            for(int i = 0; i < 3; i++)
+            {
+                vetor3D[i] = i;
+            }
+            return vetor3D;
+        }
+        public static void MostrarVetor(int[] vetorResp)
+        {
+            for(int i = 0; i < vetorResp.Length; i++)
+            {
+                Console.WriteLine("[" + vetorResp[i] + "]");
+            }
+        }
+    }
+}
